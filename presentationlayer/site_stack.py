c1575286c10034afc084cfa3c23703f771923ba1
Kobbyjust <sagarinokoe1@gmail.com>
2022-12-05 15:54:27 +0000

    First Commit

--- presentationlayer/site_stack.py
+++ presentationlayer/site_stack.py
@@ -9,13 +9,11 @@ class StaticSiteStack(Stack):
         site = StaticSitePublicS3(
                 self,
                 f"{props['namespace']}-construct",
-                # site_domain_name=site_domain_name,
-                # domain_certificate_arn=props["domain_certificate_arn"],
+
                 origin_referer_header_parameter_name=props[
                     "origin_custom_header_parameter_name"
                 ],
-                # hosted_zone_id=props["hosted_zone_id"],
-                # hosted_zone_name=props["hosted_zone_name"],
+               
             )
 
         # Add stack outputs
@@ -30,50 +28,4 @@ class StaticSiteStack(Stack):
             value=site.distribution.distribution_id,
         )
 
-#         site_domain_name = props["domain_name"]
-#         if props["sub_domain_name"]:
-#             site_domain_name = (
-#                 f'{props["sub_domain_name"]}.{props["domain_name"]}'
-#             )
-
-#         # If S3 website endpoint enabled, it creates the static site using a
-#         # public S3 as the origin. Otherwise, it creates a private S3 as the
-#         # origin.
-#         if props["enable_s3_website_endpoint"]:
-#             site = StaticSitePublicS3(
-#                 self,
-#                 f"{props['namespace']}-construct",
-#                 site_domain_name=site_domain_name,
-#                 domain_certificate_arn=props["domain_certificate_arn"],
-#                 origin_referer_header_parameter_name=props[
-#                     "origin_custom_header_parameter_name"
-#                 ],
-#                 hosted_zone_id=props["hosted_zone_id"],
-#                 hosted_zone_name=props["hosted_zone_name"],
-#             )
-#         else:
-#             site = StaticSitePrivateS3(
-#                 self,
-#                 f"{props['namespace']}-construct",
-#                 site_domain_name=site_domain_name,
-#                 domain_certificate_arn=props["domain_certificate_arn"],
-#                 hosted_zone_id=props["hosted_zone_id"],
-#                 hosted_zone_name=props["hosted_zone_name"],
-#             )
-
-#         # Add stack outputs
-#         CfnOutput(
-#             self,
-#             "SiteBucketName",
-#             value=site.bucket.bucket_name,
-#         )
-#         CfnOutput(
-#             self,
-#             "DistributionId",
-#             value=site.distribution.distribution_id,
-#         )
-#         CfnOutput(
-#             self,
-#             "CertificateArn",
-#             value=site.certificate.certificate_arn,
-#         )
+#
