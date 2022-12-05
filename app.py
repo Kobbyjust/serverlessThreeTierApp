a4c39555b52b94ed49ecc43b37fbffa527edc779
Kobbyjust <sagarinokoe1@gmail.com>
2022-12-05 15:46:14 +0000

    First Commit

--- app.py
+++ app.py
@@ -3,9 +3,37 @@
 import aws_cdk as cdk
 
 from serverless_three_tier_app.serverless_three_tier_app_stack import ServerlessThreeTierAppStack
-
+from presentationlayer.frontend import StaticSitePublicS3
+from presentationlayer.site_stack import StaticSiteStack
 
 app = cdk.App()
-ServerlessThreeTierAppStack(app, "serverless-three-tier-app")
+# ServerlessThreeTierAppStack(app, "serverless-three-tier-app")
+
+props = {
+    "namespace": app.node.try_get_context("namespace"),
+    "domain_name": app.node.try_get_context("domain_name"),
+    "sub_domain_name": app.node.try_get_context("sub_domain_name"),
+    "domain_certificate_arn": app.node.try_get_context(
+        "domain_certificate_arn"
+    ),
+    "enable_s3_website_endpoint": app.node.try_get_context(
+        "enable_s3_website_endpoint"
+    ),
+    "origin_custom_header_parameter_name": app.node.try_get_context(
+        "origin_custom_header_parameter_name"
+    ),
+    "hosted_zone_id": app.node.try_get_context("hosted_zone_id"),
+    "hosted_zone_name": app.node.try_get_context("hosted_zone_name"),
+}
+
+# presentationLayer = StaticSitePublicS3(
+#     app, "my s3 and cloudfront" 
+# )
+StaticSite = StaticSiteStack(
+    scope=app,
+    construct_id=f"{props['namespace']}-stack",
+    props=props,
+    description="Static Site using S3, CloudFront and Route53",
+)
 
 app.synth()
